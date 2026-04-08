e34f9224546e26ff61076dd737d18c16a05fad18
AJ D <ajdibo23@gmail.com>
2026-04-08 15:32:05 -0500

    test

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ import express from 'express';
 import fetch from 'node-fetch';
 import bodyParser from 'body-parser';
 import fs from 'fs';
+import updateBoard from './updateBoard.js';
 
+let autoUpdateInterval = null;
 let data = JSON.parse(fs.readFileSync('data.json', 'utf8'));
 const app = express();
 app.use(bodyParser.json());
@@ -15,6 +17,14 @@ app.get('/:file', (req, res) => {
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/api/settings', (req, res) => {
+    res.json(data['settings']);
+});
+
+app.get('/api/user', (req, res) => {
+    res.json(data['user']);
 });
 
 app.post('/api/search', (req, res) => {
@@ -31,7 +41,38 @@ app.post('/api/search', (req, res) => {
 app.post('/api/save', (req, res) => {
     data['settings'] = req.body;
     fs.writeFileSync('data.json', JSON.stringify(data, null, 2));
+    if (data['settings']['autoUpdate'] == true) {
+        manageAutoUpdate();
+    }
     res.json({ message: 'Data saved successfully' });
+});
+
+function manageAutoUpdate() {
+    if (data['settings']['autoUpdate'] == false) {
+        if (autoUpdateInterval) {
+            clearInterval(autoUpdateInterval);
+        }
+        return;
+    }
+
+    autoUpdateInterval = setInterval(() => {
+        fetch('https://mixerno.space/api/youtube-channel-counter/user/' + data['settings']['id'])
+            .then(res => res.json())
+            .then(data => {
+                data['user'] = data;
+                fs.writeFileSync('data.json', JSON.stringify(data, null, 2));
+                updateBoard(data);
+            });
+    }, parseInt(data['settings']['interval']) * 1000);
+}
+
+updateBoard({
+    'settings': {
+        'color': '#ff0000'
+    },
+    'user': {
+        'count': 1234567890
+    }
 });
 
 app.listen(4000, () => {
---
+++ updateBoard.js
@@ -0,0 +1,59 @@
+const ws281x = require('/home/aj/led-sub-count/node-rpi-ws281x-native');
+const numbers = [
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12],          // "0"
+    [0, 1, 2, 3, 4],                          // "1"
+    [0, 1, 2, 12, 4, 5, 6, 7, 8, 10, 11],           // "2"
+    [0, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12],           // "3"
+    [2, 3, 4, 6, 7, 8, 9, 10, 11],                // "4"
+    [0, 11, 10, 9, 8, 12, 2, 3, 4, 5, 6],           // "5"
+    [0, 1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12],         // "6"
+    [4, 5, 6, 7, 8, 9, 10],                     // "7"
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],       // "8"
+    [0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]          // "9"
+];
+
+const LED_COUNT = 130;
+const channel = ws281x(LED_COUNT, {
+    gpioPin: 18,
+    brightness: 255,
+    stripType: ws281x.stripType.WS2812,
+});
+
+const pixels = channel.array;
+pixels.fill(0);
+
+for (let i = 0; i < 20; i++) {
+    pixels[i] = 0x0000ff00; // red in WRGB format
+}
+
+ws281x.render();
+console.log('First 20 LEDs set to red.');
+
+process.on('SIGINT', () => {
+    pixels.fill(0);
+    ws281x.render();
+    ws281x.reset();
+    process.exit(0);
+});
+
+function updateBoard(data) {
+    resetBoard();
+    let count = data['user']['count'];
+    let color = data['settings']['color'];
+
+    for (let i = 0; i < count.toString().length; i++) {
+        let number = count.toString()[i];
+        let numberArray = numbers[number];
+        for (let j = 0; j < numberArray.length; j++) {
+            pixels[i * 13 + numberArray[j]] = color;
+        }
+    }
+    ws281x.render();
+}
+
+function resetBoard() {
+    pixels.fill(0);
+    ws281x.render();
+}
+
+export default updateBoard;
